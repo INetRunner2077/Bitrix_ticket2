2af5db51df0368be2e51b67cc6c12c2e134d0b5b
INetRunner2077 <danildro1999@gmail.com>
2023-03-25 20:58:06 +0300

    [ex2-102] Добавить англоязычную версию сайта (один инфоблок).

---
+++ local/templates/ex2_multilang_template_materials/components/bitrix/news.list/template1/template.php
@@ -0,0 +1,41 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="news-list">
+<?foreach($arResult["ITEMS"] as $arItem):?>
+	<?
+	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	?>
+	<p class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
+			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
+		<?endif?>
+		<?if($arItem['PROPERTIES']['EN_NAME']['VALUE'] ):?>
+			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem['PROPERTIES']['EN_NAME']['VALUE'] ?></b></a><br />
+			<?else:?>
+				<b><?echo $arItem["NAME"]?></b><br />
+			<?endif;?>
+		<?endif;?>
+		<?if($arItem['PROPERTIES']['PREV_NAME']['VALUE']['TEXT']):?>
+			<?echo $arItem['PROPERTIES']['PREV_NAME']['VALUE']['TEXT'];?>
+		<?endif;?>
+		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+			<div style="clear:both"></div>
+		<?endif?>
+	</p>
+
+<?endforeach;?>
+</div>
